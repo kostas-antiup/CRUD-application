dbd513f0c6d029c0739f08f17f1a60613c3fe7f7
Kostas Ragauskas <kostas@aai-labs.com>
2025-10-01 14:46:54 +0300

    Config update

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ ROOT_DIR = Path(__file__).parent
 STATIC_CONFIG = {
     "DATABASE_URL": os.getenv("DATABASE_URL"),
     "db_path": "records.db",
+    "backend_port": int(os.getenv("PORT", 8033)),
 }
